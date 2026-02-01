4aaf7215869facac2dc7469bd76905406d9521be
men6d656e <men6d656e@gmail.com>
2026-02-01 23:06:29 +0500

    production api url added in moile api for correct location

--- 04_twitter_clone/mobile/utils/api.ts
+++ 04_twitter_clone/mobile/utils/api.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 import { useAuth } from "@clerk/clerk-expo";
 
-// const API_BASE_URL = "https://react-native-bootcamp-w4vc.vercel.app/api";
-const API_BASE_URL = "https://c609a420a8af.ngrok-free.app/api";
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const createApiClient = (
   getToken: () => Promise<string | null>,
